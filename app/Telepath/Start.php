9ae76e967e5c4d57806f7b690596e5344fd81633
Tii <mail@tii.one>
2023-03-22 14:17:09 +0100

    Start message :)

---
+++ app/Telepath/Start.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Telepath;
+
+use App\Telepath\Middleware\OnlyAuthorizedUsers;
+use Telepath\Bot;
+use Telepath\Handlers\Message\Command;
+use Telepath\Middleware\Attributes\Middleware;
+use Telepath\Telegram\Update;
+
+#[Middleware(OnlyAuthorizedUsers::class)]
+class Start
+{
+
+    public function __construct(
+        protected Bot $bot,
+    ) {}
+
+    #[Command('start')]
+    public function __invoke(Update $update)
+    {
+        $name = $update->message->from->first_name;
+
+        $this->bot->sendMessage(
+            chat_id: $update->message->chat->id,
+            text: <<<EOT
+👋 Hallo, $name.
+Ich stehe dir gerne zur Verfügung.
+💬 Leite mir dazu einfach eine Sprachnachricht weiter und ich transkribiere sie für dich.
+🚗 🏗️ 🔊 Störende Hintergrundgeräusche entferne ich dabei auch gleich mit.
+
+⚠️ 💰 Aber bitte behalte im Kopf, dass jede Minute Geld kostet.
+EOT
+        );
+    }
+
+}
