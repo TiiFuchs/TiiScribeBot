420e0056775f93bfdea65b5649d4ebfd6015b350
Tii <mail@tii.one>
2023-03-23 22:37:00 +0100

    Fix summarize function and prohibit pressing multiple times

--- app/Telepath/Summarize.php
+++ app/Telepath/Summarize.php
@@ -3,8 +3,10 @@
 namespace App\Telepath;
 
 use App\Telepath\Middleware\Can;
+use Illuminate\Support\Facades\Cache;
 use OpenAI\Client;
 use Telepath\Bot;
+use Telepath\Exceptions\TelegramException;
 use Telepath\Handlers\CallbackQuery\CallbackQueryData;
 use Telepath\Middleware\Attributes\Middleware;
 use Telepath\Telegram\InlineKeyboardMarkup;
@@ -21,21 +23,28 @@ class Summarize
     #[CallbackQueryData(exact: 'summarize')]
     public function summarize(Update $update)
     {
-        $text = $update->callback_query->message->text;
-        $summary = $this->generateSummary($text);
+        $message = $update->callback_query->message;
+
+        $cacheKey = "summarize:{$message->chat->id}:{$message->message_id}";
+        if (Cache::has($cacheKey)) {
+            return;
+        }
+        Cache::set($cacheKey, true, now()->addSeconds(30));
+
+        $summary = $this->generateSummary($message->text);
 
         // Send summary
         $this->bot->sendMessage(
-            chat_id: $update->callback_query->message->chat->id,
-            reply_to_message_id: $update->callback_query->message->message_id,
+            chat_id: $message->chat->id,
             text: "<b>Zusammenfassung:</b>\n" . $summary,
             parse_mode: 'HTML',
+            reply_to_message_id: $message->message_id,
         );
 
         // Remove button
         $this->bot->editMessageReplyMarkup(
-            chat_id: $update->callback_query->message->chat->id,
-            message_id: $update->callback_query->message->message_id,
+            chat_id: $message->chat->id,
+            message_id: $message->message_id,
             reply_markup: InlineKeyboardMarkup::make(
                 [[]]
             )
